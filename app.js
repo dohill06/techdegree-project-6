e4118139a680758068248f080c3b6b6edf213280
Douglas Hill <dohill06@gmail.com>
2019-05-01 13:04:26 -0700

    Set up pug

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.listen(3000, () => {
     console.log('The app is running on localhost:3000')
 });
