a252285c8fd192c5b312e522174a9828b50477ac
Douglas Hill <dohill06@gmail.com>
2019-05-02 13:13:20 -0700

    Set up 'index' route to render 'Home' page

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const projects = data.projects;
 
 app.set('view engine', 'pug');
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 app.listen(3000, () => {
     console.log('The app is running on localhost:3000')
 });
