e3945b46228d37254313c73b057fd64231b86c48
Douglas Hill <dohill06@gmail.com>
2019-05-03 12:07:42 -0700

    Set up error handling

--- app.js
+++ app.js
@@ -20,6 +20,18 @@ app.get('/project/:id', (req, res) => {
     res.render('project', {project: projects[req.params.id]});
 });
 
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
+
 app.listen(3000, () => {
     console.log('The app is running on localhost:3000')
 });
