287c2e8be081a636235fcba2cfee42d633e50130
Douglas Hill <dohill06@gmail.com>
2019-05-03 08:52:25 -0700

    Add the 'project' route

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
+app.get('/project/:id', (req, res) => {
+    res.render('project');
+});
+
 app.listen(3000, () => {
     console.log('The app is running on localhost:3000')
 });
